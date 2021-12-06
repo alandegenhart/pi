5817136134f33034f8d3633e9f3896c3c31840d5
alandegenhart <alandegenhart@gmail.com>
2021-12-05 18:09:57 -0800

    Adding button

--- examples/gpio_test.py
+++ examples/gpio_test.py
@@ -8,13 +8,23 @@ import gpiozero
 def main():
     """Main display function."""
     led = gpiozero.LED(17)
+    button = gpiozero.Button(22)
 
     # Turn on and off LED
-    for i in range(10):
+    for i in range(5):
         led.on()
-        time.sleep(1)
+        time.sleep(0.25)
         led.off()
-        time.sleep(1)
+        time.sleep(0.25)
+
+    # Test button
+    while True:
+        if button.is_pressed:
+            button_state = 'pressed'
+        else:
+            button_state = 'not pressed'
+
+        print(f'Button state: {button_state}')
 
     return None
 
