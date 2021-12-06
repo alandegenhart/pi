5e54c6f76ab2a4d65da8c9aabee2eb213bf8deb8
alandegenhart <alandegenhart@gmail.com>
2021-12-05 18:20:21 -0800

    Adding relay class

--- examples/gpio_test.py
+++ examples/gpio_test.py
@@ -6,10 +6,24 @@ import gpiozero
 import signal
 
 
+class Relay():
+    def __init__(self, pin):
+        self.led = gpiozero.LED(pin)
+
+    def on():
+        """Turn on LED."""
+        self.led.on()
+
+    def off():
+        """Turn off LED."""
+        self.led.off()
+
+
 def main():
     """Main display function."""
     led = gpiozero.LED(17)
     button = gpiozero.Button(22)
+    relay = Relay(17)
 
     # Turn on and off LED
     for i in range(5):
@@ -18,9 +32,9 @@ def main():
         led.off()
         time.sleep(0.25)
 
-    # Test button
-    button.when_pressed = led.on
-    button.when_released = led.off
+    # Initialize button action
+    button.when_pressed = relay.on
+    button.when_released = relay.off
 
     signal.pause()
 
